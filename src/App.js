a3f471cfb595aa1ee4a606aa60cd6e0809fbe9d9
aphila1 <phly.mjo@gmail.com>
2026-05-18 15:21:18 +0200

    Completed splash screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import Write from "./Components/Write";
+import SplashScreen from "./SplashScreen";
+import './SplashScreen';
+import { useState, useEffect } from "react";
+
 
 
 function App() {
+  //Create a state variable called "showSplash" to control the visibility of the splash screen.
+  const [isLoading, setIsLoading] =useState(true);
+
+  //use "useEffect" to set a timer that will change the "showSplash" state to false after 3 seconds (3000 milliseconds).
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
+  //Conditional rendering is used to display the SplashScreen component while "showSplash" is true, and the main application content (the Router and Routes) when "showSplash" is false.
+  if (isLoading) {
+    return <SplashScreen />;
+  }
   return (
     <div className="App">
       <Router>
---
+++ src/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SplashScreen.css';
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <h1>Welcome to My App</h1>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default SplashScreen;
